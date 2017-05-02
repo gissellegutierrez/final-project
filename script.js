8a43ddbbc7517deb69706d0fb716fdb7019e3e3e
kyonori1 <reustache2019@bklawtech.com>
2017-05-02 21:36:21 +0000

    includes javascript and set up click handler

---
+++ script.js
@@ -0,0 +1,18 @@
+ console.log('helloss')
+ 
+$(function(){
+     $("#start").click(function () {
+     console.log('hello')
+     var offers = $("#phd").val();
+     
+     console.log(offers)
+ });
+     
+ 
+//  /* $.getJSON(
+
+//         "https://nearbycolleges.info/api/search?key=1465d1cd-e9fc-4011-9208-7b2df521e9fc" + offers + "&units=imperial&appid=1a8973d871a7d4763499d364d0a781ed",
+
+
+//     );''
+}) 
